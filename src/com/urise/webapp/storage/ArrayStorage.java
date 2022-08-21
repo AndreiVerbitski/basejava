ec7adc50a203010cec445e09dcdfcf35c5706cc7
Andrei <andrew162555@gmail.com>
2022-08-21 13:03:52 +0300

    Правка HW2.

--- src/com/urise/webapp/storage/ArrayStorage.java
+++ src/com/urise/webapp/storage/ArrayStorage.java
@@ -20,7 +20,7 @@ public class ArrayStorage {
     public void update(Resume r) {
         int index = getSearchKey(r.getUuid());
 
-        if (storage[index] == r) {
+        if (index >= 0) {
             storage[index] = r;
         } else {
             System.out.println("Данного резюме " + r.getUuid() + " нет в списке.");
@@ -32,8 +32,8 @@ public class ArrayStorage {
 
         if (size >= storage.length) {
             System.out.println("Список переполнен.");
-        } else if (index >= 0 && storage[index] == r) {
-            throw new ArrayIndexOutOfBoundsException("Данное резюме " + r.getUuid() + " уже есть в списке.");
+        } else if (index >= 0) {
+            System.out.println("Данное резюме " + r.getUuid() + " уже есть в списке.");
         } else {
             storage[size] = r;
             size++;
@@ -43,14 +43,10 @@ public class ArrayStorage {
     public Resume get(String uuid) {
         int index = getSearchKey(uuid);
 
-        try {
-            if (storage[index].getUuid().equals(uuid)) {
-                return storage[index];
-            } else {
-                throw new ArrayIndexOutOfBoundsException("Данное резюме " + uuid + " уже есть в списке.");
-            }
-        } catch (ArrayIndexOutOfBoundsException e) {
-            System.out.println("Данного резюме " + uuid + " нет в списке.");
+        if (index >= 0) {
+            return storage[index];
+        } else {
+            System.out.println("Данное резюме " + uuid + " уже есть в списке.");
         }
         return null;
     }
@@ -58,7 +54,7 @@ public class ArrayStorage {
     public void delete(String uuid) {
         int index = getSearchKey(uuid);
 
-        if (storage[index].equals(uuid)) {
+        if (index >= 0) {
             storage[index] = storage[size - 1];
             storage[size - 1] = null;
             size--;
