ee611f411920cab0d6814427c2a204315665f86d
Andrei <andrew162555@gmail.com>
2022-08-29 02:04:40 +0300

    Правка HW3.

--- src/com/urise/webapp/storage/SortedArrayStorage.java
+++ src/com/urise/webapp/storage/SortedArrayStorage.java
@@ -3,23 +3,15 @@ package com.urise.webapp.storage;
 import com.urise.webapp.model.Resume;
 
 import java.util.Arrays;
+import java.util.Comparator;
 
 public class SortedArrayStorage extends AbstractArrayStorage {
 
     @Override
     protected int getSearchKey(String uuid) {
-        for (int i = 0; i < size; i++) {
-            for (int j = 0; i < size; j++) {
-                if (storage[j].getUuid().compareTo(storage[j + 1].getUuid()) > 0) {
-                    Resume temp = storage[i];
-                    storage[i] = storage[j];
-                    storage[j] = temp;
-                }
-            }
-        }
         Resume searchKey = new Resume();
         searchKey.setUuid(uuid);
-        return Arrays.binarySearch(storage, 0, size, searchKey);
+        return Arrays.binarySearch(storage, 0, size, searchKey, Comparator.comparing(Resume::getUuid));
     }
 
     @Override
